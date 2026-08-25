34d4ed27f576df1dcfb680c8abc635761bc5af9b
Zixuan <szx0726@gmail.com>
2026-08-25 05:18:49 -0700

    Unity project setup

--- Source/Yourscraft/Yourscraft.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Yourscraft.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Yourscraft, "Yourscraft" );
-
-DEFINE_LOG_CATEGORY(LogYourscraft)
